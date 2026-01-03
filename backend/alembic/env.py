0edda2e7794e610a1224435d4ca27584dd5923c2
Milosmithy58 <149161897+Milosmithy58@users.noreply.github.com>
2026-01-03 20:37:53 +0000

    chore: fix alembic config and CI (#17)

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -18,6 +18,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option("sqlalchemy.url", get_database_url())
+
 target_metadata = Base.metadata
 
 
